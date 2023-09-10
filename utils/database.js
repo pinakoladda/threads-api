37c69c60b9b3c8b490bfd936552a8379e4ae5aed
pinakoladda <pinakoladda8@gmail.com>
2023-09-10 14:00:24 +0400

    style: fix typo

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/threds')
-    .then(() => {console.log('connected to database')})
-    .catch((err) => {console.error(err)})
-}
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/threads")
+    .then(() => {
+      console.log("connected to database");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
