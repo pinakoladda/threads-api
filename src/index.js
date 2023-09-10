d2429be46ee5ab81db5688a62b38ca89b9054c4a
pinakoladda <pinakoladda8@gmail.com>
2023-09-10 14:00:24 +0400

    feat: added eslint & prettier

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { connectDB } from "../utils/database.js";
+
+const app = express();
+connectDB();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
