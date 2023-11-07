74ff2dba64d6d207f158b427510f59e8a5003db6
pinakoladda <pinakoladda8@gmail.com>
2023-11-07 17:00:24 +0400

    feat: create user posts endpoint

--- routes/post.js
+++ routes/post.js
@@ -7,6 +7,7 @@ import {
   updatePost,
   likePost,
   dislikePost,
+  getUserPosts,
 } from "../controllers/post.js";
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.delete("/:id", deletePost);
 router.patch("/:id", updatePost);
 router.put("/:id/like", likePost);
 router.delete("/:id/like", dislikePost);
+router.get("/:userId", getUserPosts);
 
 export default router;
